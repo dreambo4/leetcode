8070e5b6dcfdc85d271e8666ac6c8175eba672a7
YR <dreambo4@gmail.com>
2022-04-25 22:17:13 +0800

    1-update

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -12,32 +12,18 @@ import java.util.Map;
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, ArrayList<Integer>> table = new HashMap<>();
+        HashMap<Integer, Integer> table = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            ArrayList<Integer> list;
-            if(!table.containsKey(num)) {
-                list = new ArrayList<>();
-            } else {
-                list = table.get(num);
-            }
-            list.add(i);
-            table.put(num, list);
-        }
-
-        for (Map.Entry<Integer, ArrayList<Integer>> entry : table.entrySet()) {
-            int firstNum = entry.getKey();
+            int firstNum = nums[i];
             int secondNum = target - firstNum;
 
-            if (!table.containsKey(secondNum)) continue;
-            
-            if (secondNum == firstNum) {
-                return new int[]{entry.getValue().get(0), entry.getValue().get(1)};
+            if(!table.containsKey(secondNum)) {
+                table.put(firstNum, i);
             } else {
-                return new int[]{entry.getValue().get(0), table.get(secondNum).get(0)};
+                return new int[]{i, table.get(secondNum)};
             }
         }
-    
+
         return new int[]{};
     }
 }
